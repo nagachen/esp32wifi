d950ce930147f33ac6f7d77f3e1d41c0c1ea47bf
nagachen <nagachen@example.com>
2025-11-25 09:55:59 +0800

    src ok

--- wifiscan/src/main.cpp
+++ wifiscan/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "Google8aaa";
+const char *passwdord = "5161naga";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, passwdord);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi_Connected");
+  Serial.print("My IP=");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Submask=");
+  Serial.println(WiFi.subnetMask());
+  {
+    /* code */
+  }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
